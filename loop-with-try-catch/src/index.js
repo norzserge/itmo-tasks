9591730645c8d676cb9c5490fc4f5a02db3e6b33
smetelskiy <smetelskiy@axiomsl.com>
2020-01-24 16:12:31 +0300

    task complete

--- loop-with-try-catch/src/index.js
+++ loop-with-try-catch/src/index.js
@@ -1,11 +1,20 @@
 console.clear();
-let i = 2;
-let num = 4;
-let isPrime = true;
 
-while(i <= Math.floor(Math.sqrt(num)) && isPrime == true) {
-	if (num % i == 0) { isPrime = false };
-	i++;
+const isPrimeNum = (num = 11) => {
+	let i = 2;
+	let isPrime = true;
+	if (num < 2 || num > 1000000) throw new Error (`Введеное значение "${num}" должно быть больше 1 и меньше 1 000 000!`);
+	while(i <= Math.floor(Math.sqrt(num)) && isPrime == true) {
+		if (num % i == 0) { isPrime = false };
+		i++;
+	}
+	console.log(`Число ${num} ${isPrime ? '- простое' : '- составное'}`);
+};
+
+try {
+	isPrimeNum(1);
+} catch(e) {
+	console.log(`${e.name}: ${e.message}`);
+} finally {
+	console.log('Программа выполнена');
 }
-
-console.log(`Число ${num} ${isPrime ? 'простое' : 'составное'}`);
