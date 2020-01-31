f8999d4ed231eb10c05a2a3bdb07d59e3b237212
smetelskiy <smetelskiy@axiomsl.com>
2020-01-31 14:04:23 +0300

    example with counter

--- bubble/src/index.js
+++ bubble/src/index.js
@@ -1,5 +1,8 @@
 let sender = document.querySelector('.sender');
 let recepient = document.querySelector('.recepient');
+let button = document.querySelector('.counter-btn');
+let counterNum = document.querySelector('.counter');
+let counter = 0;
 
 // создаем кастомное событие и колбэк из функции, которая добавляет в блок эмодзи-символ, взятый из title
 recepient.addEventListener('myCustomEvent', e => e.target.children[1].textContent += e.target.title);
@@ -10,3 +13,10 @@ sender.addEventListener('click', () => recepient.dispatchEvent(new CustomEvent('
 // создаем обмен событиями между двумя элементами
 sender.addEventListener('mySecondCustomEvent', e => e.target.textContent += e.target.title);
 recepient.addEventListener('click', () => sender.dispatchEvent(new CustomEvent('mySecondCustomEvent')));
+
+// Пример №2 - при клике по кнопке увеличиваем счетчик добавленных в корзину товаров
+counterNum.addEventListener('counterIncrease', e => {
+    counter++;
+    e.target.textContent = counter;
+});
+button.addEventListener('click', () => counterNum.dispatchEvent(new CustomEvent('counterIncrease')));
