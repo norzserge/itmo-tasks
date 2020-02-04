69c6ae00f8105610afb78bde8863620aa772d42d
smetelskiy <smetelskiy@axiomsl.com>
2020-02-04 18:53:38 +0300

    data output in client

--- array-sort-table/script.js
+++ array-sort-table/script.js
@@ -1,11 +1,28 @@
 const url = 'https://kodaktor.ru/j/ips';
+let dataObject, 
+    dataArraySortable = [],
+    table = document.querySelector('.sort-table tbody'),
+    tableContent = '';
 
 let promise = fetch(url);
 promise.then(response => response.ok ? response.json() : console.log(`HTTP error: ${response.status}`))
 .then(result => {
-    let res = result.reduce((acc, element) => ({
+    dataObject = result.reduce((acc, element) => ({
         ...acc, [element.ip]: (acc[element.ip] || 0) + 1
     }), {});
-    console.log(res);
+    
+    for (let ip in dataObject) {
+        dataArraySortable.push([ip, dataObject[ip]]);
+    }
+
+    dataArraySortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    dataArraySortable.forEach(element => {
+        tableContent += `<tr><td>${element[1]}</td><td>${element[0]}</td></tr>`;
+    });
+   
+    table.innerHTML = tableContent;
 })
 .catch(err => console.error(err));
