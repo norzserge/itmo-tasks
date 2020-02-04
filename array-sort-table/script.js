2aded831a092dc7f76b09c9af1da734f55788632
smetelskiy <smetelskiy@axiomsl.com>
2020-02-04 15:54:45 +0300

    reduce added

--- array-sort-table/script.js
+++ array-sort-table/script.js
@@ -3,6 +3,9 @@ const url = 'https://kodaktor.ru/j/ips';
 let promise = fetch(url);
 promise.then(response => response.ok ? response.json() : console.log(`HTTP error: ${response.status}`))
 .then(result => {
-    console.log(result);
+    let res = result.reduce((acc, element) => ({
+        ...acc, [element.ip]: (acc[element.ip] || 0) + 1
+    }), {});
+    console.log(res);
 })
 .catch(err => console.error(err));
