c7f43fd30ccd865467ffea5d8affe2d9544d6606
norzserge <metelskiisa@mail.ru>
2020-02-10 21:36:10 +0300

    task complete

--- remove-object-item/script.js
+++ remove-object-item/script.js
@@ -5,13 +5,12 @@ let listItems = '';
 let promise = fetch(url);
 promise.then(response => response.ok ? response.json() : console.log('Problem with response. Status code: ' + response.status))
 .then(result => {
-    result.users.forEach((element, index) => {
-    //    listItems += `<li>${element.login}</li>`;
-    //    list.innerHTML = listItems;
-        if(element.login === "myuserlogin@my.spb.ru") {
-            delete result.users[index];
+    result.users.forEach((element, i) => {
+        if(element.login === 'myuserlogin@my.spb.ru') {
+            result.users.splice(i, 1);
         }
+        listItems += `<li>login: ${element.login} password: ${element.password}</li>`;   
     });
-    console.log(result.users);
+    list.innerHTML = listItems;
 })
 .catch(err => console.error(err));
